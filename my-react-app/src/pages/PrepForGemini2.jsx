e33fd0c4383ebff2a4558fbc94f0b7bb3ad75f08
Tammer Kamel <tammer@tammer.com>
2026-01-25 22:00:23 -0500

    typo

--- my-react-app/src/pages/PrepForGemini2.jsx
+++ my-react-app/src/pages/PrepForGemini2.jsx
@@ -403,7 +403,7 @@ function PrepForGemini2({ navigate }) {
           </button>
           <span role="tooltip" className="title-tooltip-content">
             Search by meeting name, date or meeting ID.
-            For date search, us YYYY-MM-DD format.
+            For date search, use YYYY-MM-DD format.
 
           </span>
         </span>
